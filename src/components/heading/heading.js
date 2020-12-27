348b9658e9cc034a7b0cb1aec0d0c91885632e7d
Ramvishvas Kumar <ramvishvasg@gmail.com>
2020-12-27 10:36:09 +0530

    lesson 4 - added clean-webpack-plugin to weback

---
+++ src/components/heading/heading.js
@@ -0,0 +1,15 @@
+import './styles.scss';
+
+class Heading {
+    h1Class = 'heading';
+
+    render() {
+        const h1Element = document.createElement('h1');
+        h1Element.innerHTML = 'Webpack is Awesome';
+        h1Element.classList.add(this.h1Class);
+        const body = document.querySelector('body');
+        body.appendChild(h1Element);
+    }
+}
+
+export default Heading;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         // path: './dist',
         path: path.resolve(__dirname, 'dist'),
         // prefix to the assets
@@ -47,6 +48,7 @@ module.exports = {
     },
     plugins: [
         new TerserPlugin(),
-        new MiniCssExtractPlugin({ filename: 'style.css' }),
+        new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
+        new CleanWebpackPlugin(),
     ],
 };
